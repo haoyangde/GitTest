4630aad8cadf050279413ce52816301286a8feaf
chensongqi <chensongqi@zhongan.com>
2018-12-28 20:57:38 +0800

    test

---
+++ Hhaha/Hhaha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hhaha
+//
+//  Created by Benson on 2018/12/28.
+//  Copyright © 2018 Benson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hhaha/Hhaha/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Hhaha
+//
+//  Created by Benson on 2018/12/28.
+//  Copyright © 2018 Benson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
